5e063b604cea75b51777d0f93384c738a9f49ad2
YARNNNYBOI <156693487+YARNNNYBOI@users.noreply.github.com>
2026-03-20 22:06:14 +0800

    gumagana na ang pedestal!!!!!
    3 days para lng sa simple na implementation

--- GUI/inventory.gd
+++ GUI/inventory.gd
@@ -2,31 +2,39 @@ extends Control
 
 @onready var artifact_grid: GridContainer = $Inventory/GridContainer
 @onready var inventory: TextureRect = $Inventory
+@onready var texture_rect: TextureRect = $Inventory/GridContainer2/TextureRect
+
+@onready var pedestals = {
+	"p1" : null,
+	"p2" : null,
+	"p3" : null,
+	"p4" : null,
+	"p5" : null,
+}
 
 func update_inventory_grid():
-	# Swap inventory texture depending on pedestal proximity
-	if InventoryScript.can_access_inventory:
-		inventory.texture = load("res://GUI/Pedestal.png")
+	
+	var key = str(GlobalScript.current_pedestal)
+	# Update pedestal display safely
+	if pedestals[GlobalScript.current_pedestal]:
+		texture_rect.texture = load(pedestals[key])
 	else:
-		inventory.texture = load("res://GUI/Inventory.png")
+		texture_rect.texture = null
+		print("No texture found for pedestal:", key)
 
 	# Clear all slots first
 	for cell in artifact_grid.get_children():
 		if cell is TextureRect:
 			cell.texture = null
 
-	# Fill cells with artifacts
-	for i in range(GlobalScript.player_artifacts.size()):
-		var tex
-		var artifact_name = GlobalScript.player_artifacts[i]
-		var number = artifact_name.split("_")[1].to_int() - 1
-		if GlobalScript.player_artifact_path.is_empty():
-			tex = null
-		else:
-			var tex_path = GlobalScript.player_artifact_path[number]
-			tex = load(tex_path)
+	# Fill cells directly using paths (NO splitting nonsense)
+	for i in range(GlobalScript.player_artifact_path.size()):
+		if i >= artifact_grid.get_child_count():
+			break
 
+		var tex_path = GlobalScript.player_artifact_path[i]
+		var tex = load(tex_path)
 
-		if i < artifact_grid.get_child_count():
-			var cell = artifact_grid.get_child(i)
+		var cell = artifact_grid.get_child(i)
+		if cell is TextureRect:
 			cell.texture = tex
--- items/pedestal.gd
+++ items/pedestal.gd
@@ -3,7 +3,11 @@ extends StaticBody2D
 
 @onready var player
 
+@export var pedestal : String
+
 func _ready() -> void:
+	add_to_group(str(pedestal))
+	print(get_groups())
 	label.visible = false
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -11,14 +15,15 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		player = body
 		label.visible = true
 		GlobalScript.on_pedestal = true
+		GlobalScript.current_pedestal = pedestal
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("players"):
 		player = null
 		label.visible = false
-		GlobalScript.on_pedestal = true
-
+		GlobalScript.on_pedestal = false
+		GlobalScript.current_pedestal = ""
 
 func _on_button_pressed() -> void:
 	print("bing")
