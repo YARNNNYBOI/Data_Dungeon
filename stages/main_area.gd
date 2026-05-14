e462f4a70af154dcbf4fcb085617117f824ddc3c
YARNNNYBOI <156693487+YARNNNYBOI@users.noreply.github.com>
2026-05-14 22:43:35 +0800

    fix major bugs

--- stages/main_area.gd
+++ stages/main_area.gd
@@ -14,6 +14,24 @@ func remove_barrier():
 	var barrier_open = GlobalScript.player_artifacts.size()
 	if barrier_open == 0 :
 		$Barriers1.queue_free()
+	elif barrier_open == 2 :
+		$Barriers2.queue_free()
+	elif barrier_open == 4 :
+		$Barriers3.queue_free()
+	elif barrier_open == 6 :
+		$Barriers4.queue_free()
+	elif barrier_open == 8 :
+		$Barriers5.queue_free()
+	elif barrier_open == 10 :
+		$Barriers6.queue_free()
+	elif barrier_open == 12 :
+		$Barriers7.queue_free()
+	elif barrier_open == 14 :
+		$Barriers8.queue_free()
+	elif barrier_open == 16 :
+		$Barriers9.queue_free()
+	elif barrier_open == 18 :
+		$Barriers10.queue_free()
 	else:
 		var current_barrier = get_node("Barriers" + str(barrier_open))
 		current_barrier.queue_free()
@@ -26,37 +44,37 @@ func _on_gate_1_body_entered(body: Node2D) -> void:
 		get_tree().change_scene_to_file("res://stages/stage_1.tscn")
 
 func _on_gate_2_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 2 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 2 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_2.tscn")
 
 func _on_gate_3_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 4 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 4 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_3.tscn")
 
 func _on_gate_4_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 6 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 6 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_4.tscn")
 
 func _on_gate_5_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 8 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 8 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_5.tscn")
 
 func _on_gate_6_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 10 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 10 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_6.tscn")
 
 func _on_gate_7_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 12 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 12 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_7.tscn")
 
 func _on_gate_8_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 14 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 14 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_8.tscn")
 
 func _on_gate_9_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 16 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 16 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_9.tscn")
 		
 func _on_gate_10_body_entered(body: Node2D) -> void:
-	if GlobalScript.player_artifacts.size() <= 18 and body.is_in_group("players"):
+	if GlobalScript.player_artifacts.size() == 18 and body.is_in_group("players"):
 		get_tree().change_scene_to_file("res://stages/stage_10.tscn")
